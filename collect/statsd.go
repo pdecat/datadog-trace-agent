440d804101c38746b23defc0f3baff61b9cbc11f
Gabriel Aszalos <gabriel.aszalos@datadoghq.com>
2018-08-10 18:42:22 +0300

    collect: add categories to gauges

--- collect/statsd.go
+++ collect/statsd.go
@@ -19,16 +19,14 @@ func (c *Cache) monitor(client statsd.StatsClient) {
 	}
 }
 
-var statsdTags = []string{"version:v1"}
-
 func (c *Cache) computeStats(client statsd.StatsClient) {
 	iter := c.newReverseIterator()
 	now := time.Now()
 	// age distribution counters
 	var (
-		n1mTo5m  float64 // age 1m-5m
-		n5mTo10m float64 // age 5m-10m
-		nOver10m float64 // age >10m
+		n5m    float64 // age 1m-5m
+		n10m   float64 // age 5m-10m
+		nLarge float64 // age >10m
 	)
 	for {
 		t, ok := iter.getAndAdvance()
@@ -40,22 +38,36 @@ func (c *Cache) computeStats(client statsd.StatsClient) {
 		case age <= time.Minute:
 			break // stop counting, these are recent spans
 		case age < 5*time.Minute:
-			n1mTo5m++
+			n5m++
 		case age < 10*time.Minute:
-			n5mTo10m++
+			n10m++
 		default:
-			nOver10m++
+			nLarge++
 		}
 	}
 
 	total := float64(iter.len())
-	client.Gauge("datadog.trace_agent.cache.ages.under_1m", total-n1mTo5m-n5mTo10m-nOver10m, statsdTags, 1)
-	client.Gauge("datadog.trace_agent.cache.ages.1m_to_5m", n1mTo5m, statsdTags, 1)
-	client.Gauge("datadog.trace_agent.cache.ages.5m_to_10m", n5mTo10m, statsdTags, 1)
-	client.Gauge("datadog.trace_agent.cache.ages.over_10m", nOver10m, statsdTags, 1)
-	client.Gauge("datadog.trace_agent.cache.count", total, statsdTags, 1)
+	client.Gauge("datadog.trace_agent.cache.traces", total-n5m-n10m-nLarge, []string{
+		"version:v1",
+		"category:under1mb",
+	}, 1)
+
+	client.Gauge("datadog.trace_agent.cache.traces", n5m, []string{
+		"version:v1",
+		"category:under5mb",
+	}, 1)
+
+	client.Gauge("datadog.trace_agent.cache.traces", n10m, []string{
+		"version:v1",
+		"category:under10mb",
+	}, 1)
+
+	client.Gauge("datadog.trace_agent.cache.traces", nLarge, []string{
+		"version:v1",
+		"category:over10mb",
+	}, 1)
 
 	c.mu.RLock()
-	client.Gauge("datadog.trace_agent.cache.bytes", float64(c.size), statsdTags, 1)
+	client.Gauge("datadog.trace_agent.cache.bytes", float64(c.size), []string{"version:v1"}, 1)
 	c.mu.RUnlock()
 }
